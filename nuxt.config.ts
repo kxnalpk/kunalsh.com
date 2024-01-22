2480a99db680ab1e1d45c1dd8ead17927fdbffc8
Kunal Sharma <hiftielabs@gmail.com>
2024-01-22 05:33:29 -0800

    Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      viewport: 'width=device-width, initial-scale=1'
     },
   }
-})
+})      
