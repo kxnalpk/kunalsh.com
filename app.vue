70351042a40dbab100a0963dde332f0518ebf271
Kunal Sharma <hiftielabs@gmail.com>
2024-01-22 21:31:42 -0800

    Faster Loading

--- app.vue
+++ app.vue
@@ -5,6 +5,7 @@ useSeoMeta({
   description: 'I\'m a 17 years old student and a developer. I write code in rust, ts & js.',
   ogDescription: 'I\'m a 17 years old student and a developer. I write code in rust, ts & js.',
 });
+
 </script>
 
 <template>
