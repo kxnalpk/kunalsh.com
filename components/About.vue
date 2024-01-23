2d6643589cdd1f7f9cb948ee6ca17e5648149f1c
Kunal Sharma <hiftielabs@gmail.com>
2024-01-22 19:47:40 -0800

    Improved the code (experience section)

--- components/About.vue
+++ components/About.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="max-w-wrapper mt-6">
         <h1 class="text-white text-2xl">About</h1>
-        <p class="text-cg-3 text-base md:text-lg">howdy? i'm a 17 years old student and a software developer. I write code in rust, ts and js. I offer web & app development services. oh and i love capybaras and marmots c:</p>
+        <p class="mt-6 text-cg-3 text-base md:text-lg">howdy? i'm a 17 years old student and a software developer. I write code in rust, ts and js. I offer web & app development services. oh and i love capybaras and marmots c:</p>
     </header>
 </template>
--- components/Experience.vue
+++ components/Experience.vue
@@ -1,12 +1,34 @@
+<script lang="ts">
+export default {
+    data() {
+        return {
+            experiences: [
+                {
+                    title: "Netcord",
+                    url: "https://netcord.in",
+                    img: "/images/netcord.png"
+                },
+                {
+                    title: "Body Shop",
+                    url: "https://thebodyshop.com",
+                    img: "/images/thebodyshop.png"
+                }
+            ]
+        };
+    }
+};
+</script>
+
 <template>
     <div class="max-w-wrapper w-full">
-        <div class="items-center justify-between mt-6 flex gap-8 bg-cg-2 p-4">
+        <h1 class="text-white text-2xl mt-6">Experience</h1>
+        <div v-for="(experience, index) in experiences" :key="index" class="items-center justify-between mt-6 flex gap-8 bg-cg-2 p-4">
             <div class="flex flex-row items-center gap-2.5">
-                <img class="w-12 rounded-full" src="/images/avatar.png"/>
-                <h1 class="text-white text-xl font-inter font-medium">Netcord</h1>
+                <img class="w-12 rounded-full" :src="experience.img"/>
+                <h1 class="text-white text-xl font-inter font-medium">{{ experience.title }}</h1>
             </div>
             <div class="items-center">
-                <a href="https://netcord.in" class="text-white p-2.5 rounded-lg bg-[#373737]">Learn More</a>
+                <a :href="experience.url" class="text-white p-2.5 rounded-lg bg-[#373737] hover:opacity-90 transition-all ease-in">Learn More</a>
             </div>
         </div>
     </div>
