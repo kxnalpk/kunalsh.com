a59ff8daba7be942794e79fb6680eb0dde8a5f86
Kunal Sharma <hiftielabs@gmail.com>
2024-01-22 23:16:32 -0800

    SEO Management

--- app.vue
+++ app.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 useSeoMeta({
   title: 'Kunal Sharma',
+  keywords: "kunalsmh",
+  themeColor: "#574585",
+  ogImage: "/images/banner.png",
+  ogSiteName: "kunalsh",
+  ogUrl: "https://kunalsh.com",
   ogTitle: 'Kunal Sharma',
   description: 'I\'m a 17 years old student and a developer. I write code in rust, ts & js.',
   ogDescription: 'I\'m a 17 years old student and a developer. I write code in rust, ts & js.',
 });
-
 </script>
 
 <template>
