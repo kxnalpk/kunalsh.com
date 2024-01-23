2fbf22d32fadc740f2e76bb99067660784d112df
Kunal Sharma <hiftielabs@gmail.com>
2024-01-22 23:26:45 -0800

    More typo fixed~

--- components/Testimonials.vue
+++ components/Testimonials.vue
@@ -6,25 +6,25 @@ export default {
                 {
                     name: "Browny",
                     username: "@legendfroggy",
-                    text: "When I first saw hiftie on DAC and CA, he was amusing and looked really good. I see him now as the same fascinating person who loves capybaras and is proficient in programming.",
+                    text: "When I first saw kunal on DAC and CA, he was amusing and looked really good. I see him now as the same fascinating person who loves capybaras and is proficient in programming.",
                     img: "/images/testimonials/browny.png",
                 },
                 {
                     name: "Rony",
                     username: "@ronykax",
-                    text: "Hiftie is an awesome developer and a pro at many other things! He makes everything look good and work great. Working with him is always fun and he's a really good friend too!",
+                    text: "Kunal is an awesome developer and a pro at many other things! He makes everything look good and work great. Working with him is always fun and he's a really good friend too!",
                     img: "/images/testimonials/rony.png",
                 },
                 {
                     name: "Dante",
                     username: "@priyobrotokar",
-                    text: "Hiftie is a creative powerhouse mastering web development and eye-catching design. He dives fearlessly into projects, whipping up impressive solutions, as well as spreading knowledge.",
+                    text: "Kunal is a creative powerhouse mastering web development and eye-catching design. He dives fearlessly into projects, whipping up impressive solutions, as well as spreading knowledge.",
                     img: "/images/testimonials/dante.png"
                 },
                 {
                     name: "Polycord",
                     username: "@polycord",
-                    text: "Hiftie is an amazing UI/UX designer and developer! He does the job in a fast speed and comes out perfect every time. I can trust him with anything and he’s also a very good friend!",
+                    text: "Kunal is an amazing UI/UX designer and developer! He does the job in a fast speed and comes out perfect every time. I can trust him with anything and he’s also a very good friend!",
                     img: "/images/testimonials/polycord.png"
                 }
             ]
