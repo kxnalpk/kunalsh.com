b7b72c7b805f466d7d0cda47c6a943b2e1702605
Kunal Sharma <hiftielabs@gmail.com>
2024-01-23 23:39:52 -0800

    yes

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <h1 class="text-white opacity-30 text-sm font-kanit text-light py-8">&copy; 2024 Kunalsmh.com. All rights reserved.</h1>
+        <h1 class="text-white opacity-80 text-sm font-kanit text-light py-8">&copy; 2024 Kunalsmh.com. All rights reserved.</h1>
     </div>
 </template>
