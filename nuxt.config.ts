ed05b880ee3b934832cdcb8dd962bb62971c622d
Kunal Sharma <hiftielabs@gmail.com>
2024-01-23 22:24:37 -0800

    Favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,8 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1'
+      viewport: 'width=device-width, initial-scale=1',
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
     },
   },
 
