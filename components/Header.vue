b02d32a9416700ccb1866aca5810c350232251df
moli <136324426+molishu@users.noreply.github.com>
2024-03-10 03:14:08 +0300

    Added online,dnd,idle modes and colors too.

--- components/Header.vue
+++ components/Header.vue
@@ -1,34 +1,3 @@
-<script lang="ts">
-
-export default {
-    data() {
-        return {
-            socials: [
-                {
-                    "img": "/images/socials/github.svg",
-                    "url": "https://github.com/kunalsmh/",
-                    "alt": "github",
-                },
-                {
-                    "img": "/images/socials/linkedin.svg",
-                    "url": "https://i-forgot",
-                    "alt": "linkedin"
-                },
-                {
-                    "img": "/images/socials/x.svg",
-                    "url": "https://twitter.com/kunalsmh",
-                    "alt": "x",
-                }
-            ]
-        };
-    },
-};
-</script>
-
-<script setup lang="ts">
-const data = useLanyard({ method: "ws", id: "853147823066578946" });
-</script>
-
 <template>
     <header class="w-full">
         <div class="gap-6 mt-6 flex flex-nowrap justify-between items-center text-white">
@@ -36,18 +5,27 @@ const data = useLanyard({ method: "ws", id: "853147823066578946" });
                 <h1 class="font-kanit font-regular text-4xl">Kunal Sharma</h1>
                 <p class="opacity-75 text-lg font-kanit font-light">Web & App Developer</p>
                 <div class="flex gap-1 mt-3">
-
                     <a :href="social.url" class="flex" v-for="(social, index) in socials" :key="index">
                         <NuxtImg width="26" :alt="social.alt" class="hover:opacity-80 w-fit max-w-[26px]" :src="social.img">
                         </NuxtImg>
                     </a>
-
-                    <div class="bg-cg-2 rounded-md px-2 py-1 text-sm text-white opacity-80 font-kanit font-base">
+                    <div :class="['bg-cg-2', 'rounded-md', 'px-2', 'py-1', 'text-sm', 'text-white', 'opacity-80', 'font-kanit', 'font-base', {
+                        'bg-cg-idle': data?.discord_status === 'idle',
+                        'bg-cg-dnd': data?.discord_status === 'dnd',
+                        'bg-cg-online': data?.discord_status === 'online',
+                        'bg-cg-2': data?.discord_status === 'offline',
+                    }]">
                         <div v-if="data?.discord_status === 'offline'">
-                            <span v-if="data?.discord_status === 'offline'">Currently Offline</span>
+                            <span>offline</span>
                         </div>
-                        <div v-else>
-                            <span>Currently Online</span>
+                        <div v-else-if="data?.discord_status === 'online'">
+                            <span>online</span>
+                        </div>
+                        <div v-else-if="data?.discord_status === 'dnd'">
+                            <span>dnd</span>
+                        </div>
+                        <div v-else-if="data?.discord_status === 'idle'">
+                            <span>idle</span>
                         </div>
                     </div>
                 </div>
@@ -55,3 +33,26 @@ const data = useLanyard({ method: "ws", id: "853147823066578946" });
         </div>
     </header>
 </template>
+
+<script setup lang="ts">
+
+const data = useLanyard({ method: "ws", id: "470904884946796544" });
+
+const socials = [
+    {
+        "img": "/images/socials/github.svg",
+        "url": "https://github.com/kunalsmh/",
+        "alt": "github",
+    },
+    {
+        "img": "/images/socials/linkedin.svg",
+        "url": "https://linkedin.com/yourusername",
+        "alt": "linkedin"
+    },
+    {
+        "img": "/images/socials/x.svg",
+        "url": "https://twitter.com/kunalsmh",
+        "alt": "x",
+    }
+];
+</script>
